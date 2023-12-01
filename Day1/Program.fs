c7305dcfddb568af539f7671775f68e98fb3fc89
codereader <greebo@angua.at>
2023-12-01 06:31:07 +0100

    Day 1 part 1

---
+++ Day1/Program.fs
@@ -0,0 +1,20 @@
+﻿open System
+open System.IO
+
+let lines = File.ReadAllLines "..\..\..\input.txt"
+
+let getLineNumber (line: String) =
+    // Filter out all non-digits from the input string
+    let digits = line.ToCharArray() |> Array.filter Char.IsDigit
+
+    // Take the first digit
+    let lastDigit = Array.last digits |> string |> int
+    let firstDigit = Array.head digits |> string |> int
+
+    firstDigit * 10 + lastDigit
+
+let allNumbers = lines |> Array.map getLineNumber
+let total = allNumbers |> Array.sum
+
+Console.WriteLine "Total of all numbers (first and last digit combined):"
+Console.WriteLine total
