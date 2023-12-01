403cb1300252ce2a627c8bc2c4f3d7b942ddcec3
codereader <greebo@angua.at>
2023-12-01 09:36:28 +0100

    Refactoring. Merge algorithms of both parts into one. Use printf instead of Console.WriteLine Use fst and snd to obtain values from tuples. Pass the sequence of possible words as argument.

--- Day1/Program.fs
+++ Day1/Program.fs
@@ -1,43 +1,35 @@
 ﻿open System
-open System.IO
 
-let lines = File.ReadAllLines "..\..\..\input.txt"
+// Part 1 is just using single digits, Part 2 is using both arrays
+let allDigits = [| (0, "0"); (1, "1"); (2, "2"); (3, "3"); (4, "4"); (5, "5"); (6, "6"); (7, "7"); (8, "8"); (9, "9") |]
+let allSpelledOutNumbers = [| (0, "zero"); (1, "one"); (2, "two"); (3, "three"); (4, "four"); (5, "five"); (6, "six"); (7, "seven"); (8, "eight"); (9, "nine"); |]
 
-let getLineNumber (line: String) =
-    // Filter out all non-digits from the input string
-    let digits = line.ToCharArray() |> Array.filter Char.IsDigit
+let lines = IO.File.ReadAllLines "..\..\..\input.txt"
 
-    // Take the first digit
-    let lastDigit = Array.last digits |> string |> int
-    let firstDigit = Array.head digits |> string |> int
+let indexIsValid ((_: int, index: int)) = index >= 0
 
-    firstDigit * 10 + lastDigit
+let getLineNumber (line: String, possibleWords: seq<Tuple<int, string>>) =
+    // First and last index of any possible word
+    let first =
+      possibleWords
+      |> Seq.map (fun (value, word) -> (value, line.IndexOf word))
+      |> Seq.filter indexIsValid
+      |> Seq.sortBy snd // sort by value
+      |> Seq.head
 
-let allNumbers = lines |> Array.map getLineNumber
-let total = allNumbers |> Array.sum
+    let last =
+      possibleWords
+      |> Seq.map (fun (value, word) -> (value, line.LastIndexOf word))
+      |> Seq.filter indexIsValid
+      |> Seq.sortByDescending snd // sort by value, descending
+      |> Seq.head
 
-Console.WriteLine "[Part 1]: Total of all numbers (first and last digit combined):"
-Console.WriteLine total
+    fst first * 10 + fst last
 
-//let allDigits = [| '0'; '1'; '2'; '3'; '4'; '5';'6'; '7';'8'; '9' |]
-let allNumberWords = 
-    [| 
-        (0, "zero"); (1, "one"); (2, "two"); (3, "three"); (4, "four"); (5, "five"); (6, "six"); (7, "seven"); (8, "eight"); (9, "nine");
-        (0, "0"); (1, "1"); (2, "2"); (3, "3"); (4, "4"); (5, "5"); (6, "6"); (7, "7"); (8, "8"); (9, "9")
-    |]
+let totalPart1 = lines |> Seq.map (fun line -> (getLineNumber (line, allDigits))) |> Seq.sum
 
-let indexIsValid ((value: int, index: int)) = index >= 0
-let takeValue ((value: int, index: int)) = value
+printfn "[Part 1]: Total of all numbers (first and last digit combined): %d" totalPart1
 
-let getLineNumberPart2 (line: String) =
-    // First and last index of any word
-    let first = allNumberWords |> Array.map (fun (value, word) -> (value, line.IndexOf word)) |> Array.filter indexIsValid |> Array.sortBy (fun (value, index) -> index) |> Array.head
-    let last = allNumberWords |> Array.map (fun (value, word) -> (value, line.LastIndexOf word)) |> Array.filter indexIsValid |> Array.sortByDescending (fun (value, index) -> index) |> Array.head
+let totalPart2 = lines |> Seq.map (fun line -> (getLineNumber (line, Seq.concat [| allSpelledOutNumbers; allDigits |]))) |> Seq.sum
 
-    takeValue first * 10 + takeValue last
-
-let allNumbersPart2 = lines |> Array.map getLineNumberPart2
-let totalPart2 = allNumbersPart2 |> Array.sum
-
-Console.WriteLine "[Part 2]: Total of all numbers including number words (first and last digit combined):"
-Console.WriteLine totalPart2
+printfn "[Part 2]: Total of all numbers including number words (first and last digit combined): %d" totalPart2
