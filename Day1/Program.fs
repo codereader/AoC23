a5760b7018adf559a5a85544236f482f2cfd7ac9
codereader <greebo@angua.at>
2023-12-01 09:04:27 +0100

    Day 1 part 2

--- Day1/Program.fs
+++ Day1/Program.fs
@@ -16,5 +16,28 @@ let getLineNumber (line: String) =
 let allNumbers = lines |> Array.map getLineNumber
 let total = allNumbers |> Array.sum
 
-Console.WriteLine "Total of all numbers (first and last digit combined):"
+Console.WriteLine "[Part 1]: Total of all numbers (first and last digit combined):"
 Console.WriteLine total
+
+//let allDigits = [| '0'; '1'; '2'; '3'; '4'; '5';'6'; '7';'8'; '9' |]
+let allNumberWords = 
+    [| 
+        (0, "zero"); (1, "one"); (2, "two"); (3, "three"); (4, "four"); (5, "five"); (6, "six"); (7, "seven"); (8, "eight"); (9, "nine");
+        (0, "0"); (1, "1"); (2, "2"); (3, "3"); (4, "4"); (5, "5"); (6, "6"); (7, "7"); (8, "8"); (9, "9")
+    |]
+
+let indexIsValid ((value: int, index: int)) = index >= 0
+let takeValue ((value: int, index: int)) = value
+
+let getLineNumberPart2 (line: String) =
+    // First and last index of any word
+    let first = allNumberWords |> Array.map (fun (value, word) -> (value, line.IndexOf word)) |> Array.filter indexIsValid |> Array.sortBy (fun (value, index) -> index) |> Array.head
+    let last = allNumberWords |> Array.map (fun (value, word) -> (value, line.LastIndexOf word)) |> Array.filter indexIsValid |> Array.sortByDescending (fun (value, index) -> index) |> Array.head
+
+    takeValue first * 10 + takeValue last
+
+let allNumbersPart2 = lines |> Array.map getLineNumberPart2
+let totalPart2 = allNumbersPart2 |> Array.sum
+
+Console.WriteLine "[Part 2]: Total of all numbers including number words (first and last digit combined):"
+Console.WriteLine totalPart2
