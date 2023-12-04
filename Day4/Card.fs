57ae6aa413fc6731ed3ab6f5381ee5cfef16429d
codereader <greebo@angua.at>
2023-12-04 17:52:38 +0100

    Day 4 part 1

---
+++ Day4/Card.fs
@@ -0,0 +1,26 @@
+﻿namespace Day4
+
+open System
+
+type Card(id, winningNumbers: Set<int>, availableNumbers: List<int>) =
+    member val Id = id
+    member val WinningNumbers = winningNumbers
+    member val AvailableNumbers = availableNumbers
+
+    new(line: string) =
+        if not (line.StartsWith("Card ")) then failwith "Line doesn't start with the Card keyword"
+
+        let parts = line.Split(':')
+        let id = Int32.Parse(parts[0].Substring("Card ".Length))
+
+        // Split into winning and available numbers ("|")
+        let numbers = parts[1].Trim().Split("|")
+
+        let winningNumbers = numbers[0].Split(' ', StringSplitOptions.RemoveEmptyEntries) |> Seq.map Int32.Parse |> Set.ofSeq
+        let availableNumbers = numbers[1].Split(' ', StringSplitOptions.RemoveEmptyEntries) |> Seq.map Int32.Parse |> List.ofSeq
+
+        Card(id, winningNumbers, availableNumbers)
+
+    member this.Value =
+        let matches = this.AvailableNumbers |> Seq.filter (fun number -> this.WinningNumbers.Contains(number)) |> Seq.length
+        if matches = 0 then 0 else (1 <<< matches - 1)
---
+++ Day4/Program.fs
@@ -0,0 +1,9 @@
+﻿open System
+open Day4
+
+let lines = IO.File.ReadAllLines @"..\..\..\input.txt"
+
+let cards = lines |> Seq.map Card
+let totalCardValue = cards |> Seq.sumBy (fun card -> card.Value)
+
+printfn "[Part 1]: Total value of all cards %d" totalCardValue
