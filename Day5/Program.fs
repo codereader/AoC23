e0639ce847477a8d616a1a59e931dadf287420f6
codereader <greebo@angua.at>
2023-12-08 17:15:11 +0100

    Refactoring

--- Day5/Program.fs
+++ Day5/Program.fs
@@ -2,22 +2,25 @@
 
 let lines = IO.File.ReadAllLines @"..\..\..\input.txt"
 
-//let seeds = "seeds: 79 14 55 13".Substring(6).Split(' ', StringSplitOptions.RemoveEmptyEntries) |> Seq.map Int64.Parse |> Seq.toArray
 let seeds = lines[0].Substring(6).Split(' ', StringSplitOptions.RemoveEmptyEntries) |> Seq.map Int64.Parse |> Seq.toList
 
 type Range(start: int64, length: int64) =
     member val Start = start
     member val Length = length
     member val End = start + length - 1L
+
     member this.Contains(point) =
         point >= this.Start && point <= this.End
+
     member this.Overlaps(other: Range) =
         this.Contains(other.Start) || this.Contains(other.End) || other.Contains(this.Start)
+
     member this.GetIntersection(other: Range) =
         if this.Overlaps(other) then
             Range(Math.Max(this.Start, other.Start), Math.Min(this.End, other.End) - Math.Max(this.Start, other.Start) + 1L)
         else
             Range(0, 0)
+
     override this.ToString() =
         sprintf "%d..%d" this.Start this.End
 
@@ -30,9 +33,6 @@ type Mapping(sourceStart: int64, destStart: int64, length: int64) =
     member this.DestinationContains(input: int64) = this.Destination.Contains(input)
 
     member this.SourceOverlaps(input: Range) = this.Source.Overlaps(input)
-
-    member this.OverlapsSourceOf(other: Mapping) =
-        this.Destination.Overlaps(other.Source)
 
     member this.Map(input: int64) =
         destStart + input - sourceStart
@@ -50,34 +50,8 @@ let ParseRange (input: string) =
     let range = input.Trim().Split(' ') |> Array.map Int64.Parse
     Mapping(range[1], range[0], range[2])
 
-let ParseMapping (input: string) = MappingCollection(input.Split("\n") |> Seq.map ParseRange |> Seq.sortBy (fun mapping -> mapping.Source.Start) |> Seq.toList)
-
-let allMappings = seq { Input.SeedToSoil; Input.SoilToFertilizer; Input.FertilizerToWater;
-    Input.WaterToLight; Input.LightToTemperature; Input.TemperatureToHumidity; Input.HumidityToLocation } |> Seq.map ParseMapping |> Seq.toList
-
-let MapSeed (inputSeed: int64) =
-    let mutable seed = inputSeed
-    allMappings |> List.iter (fun mapping -> seed <- mapping.Map seed)
-    seed
-
-let minimumLocation = seeds |> Seq.map MapSeed |> Seq.min
-printfn $"[Part 1] Minimum Location (per seed): {minimumLocation}"
-
-// Part 2: Find the lowest location interval
-let part1SeedRanges =
-    seeds
-    |> Seq.map (fun seed -> Range(seed, 1))
-    |> Seq.sortBy (fun range -> range.Start)
-
-let seedRanges =
-    seq { 0..seeds.Length / 2 - 1 }
-    |> Seq.map (fun i -> (seeds[i*2], seeds[i*2+1]))
-    |> Seq.sortByDescending fst
-    |> Seq.map Range
-
-let mutable inputSet = seedRanges |> Seq.sortBy (fun range -> range.Start)
-
-printfn "Seed Ranges: %s" (String.Join("|", inputSet))
+let ParseMapping (input: string) =
+    MappingCollection(input.Split('\n') |> Seq.map ParseRange |> Seq.sortBy (fun mapping -> mapping.Source.Start) |> Seq.toList)
 
 // Complete the mapping to fill the gaps with NOP mappings, not translating the values at all
 let CreateDenseMapping(level: MappingCollection) =
@@ -101,7 +75,7 @@ let CreateDenseMapping(level: MappingCollection) =
         // Create a range that is doing a NOP mapping
         result.Mappings <- Mapping(0, 0, mappings.Head.Source.Start) :: result.Mappings
     
-    // A final NOP mapping after the last one
+    // A final NOP mapping after the last one (this is actually not needed for my input set to compute)
     result.Mappings <- result.Mappings @ [Mapping(mappings[mappings.Length - 1].Source.End + 1L, mappings[mappings.Length - 1].Source.End + 1L,
                                Int64.MaxValue - mappings[mappings.Length - 1].Source.End - 2L)]
 
@@ -111,9 +85,6 @@ let CreateDenseMapping(level: MappingCollection) =
 let TranslateInputSet (level: MappingCollection, rangesToTransform) =
     // Fill the gaps of the incoming level mappings, the input outside the defined ranges will receive a NOP transform
     let denseMapping = CreateDenseMapping level
-
-    //denseMapping.Mappings |> Seq.iter (fun mapping -> Console.Write $"{mapping.Source}|")
-    //Console.WriteLine()
 
     // For each mapping find the ones overlapping the given range
     // Then calculate the intersection of the given range source range
@@ -132,7 +103,30 @@ let TranslateInputSet (level: MappingCollection, rangesToTransform) =
         |> Seq.toList
         )
 
-allMappings |> Seq.iter (fun level -> (inputSet <- TranslateInputSet(level, inputSet) |> Seq.toList))
+let allMappings = seq { Input.SeedToSoil; Input.SoilToFertilizer; Input.FertilizerToWater;
+    Input.WaterToLight; Input.LightToTemperature; Input.TemperatureToHumidity; Input.HumidityToLocation } |> Seq.map ParseMapping |> Seq.toList
 
-let minimumSet = inputSet |> Seq.minBy (fun set -> set.Start)
-printfn "[Part 2]: Minimum Location: %d" minimumSet.Start
+// Part 1 is using the same algorithm as part 2, but with ranges of width 1
+let part1SeedRanges =
+    seeds
+    |> Seq.sort
+    |> Seq.map (fun seed -> Range(seed, 1))
+
+// Part 2: Find the lowest location by using seed ranges
+let part2SeedRanges =
+    seq { 0..seeds.Length / 2 - 1 }
+    |> Seq.map (fun i -> (seeds[i*2], seeds[i*2+1])) // pick two numbers for each range
+    |> Seq.sortByDescending fst
+    |> Seq.map Range
+
+// Find the minimum location for the given starting set of seed ranges
+let FindMinimumLocation (seedRanges: seq<Range>) =
+    let mutable inputSet = seedRanges |> Seq.sortBy (fun range -> range.Start)
+
+    allMappings |> Seq.iter (fun level -> (inputSet <- TranslateInputSet(level, inputSet) |> Seq.toList))
+
+    let minimumSet = inputSet |> Seq.minBy (fun set -> set.Start)
+    minimumSet.Start
+
+printfn "[Part 1]: Minimum Location: %d" (FindMinimumLocation(part1SeedRanges))
+printfn "[Part 2]: Minimum Location: %d" (FindMinimumLocation(part2SeedRanges))
