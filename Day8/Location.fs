b345fcf1ea4c3a82e5eb58f320fdba2f1086597b
codereader <greebo@angua.at>
2023-12-09 17:53:55 +0100

    Day 8 Part 1

---
+++ Day8/Location.fs
@@ -0,0 +1,12 @@
+﻿namespace Day8
+
+type Location(input:string) =
+    let Pieces = input.Split(" = ")
+    let LR_Pieces = Pieces[1].Split(", ")
+    member this.Name = Pieces[0]
+    member this.Left = LR_Pieces[0].Replace("(", "").Replace(")", "")
+    member this.Right = LR_Pieces[1].Replace("(", "").Replace(")", "")
+
+    override this.ToString() =
+        sprintf "%s (L: %s | R: %s)" this.Name this.Left this.Right
+
---
+++ Day8/Program.fs
@@ -0,0 +1,37 @@
+﻿open Day8
+open System
+
+// Real puzzle input
+let lines = IO.File.ReadAllLines @"..\..\..\input.txt"
+// test input
+(*let lines = "LLR
+
+AAA = (BBB, BBB)
+BBB = (AAA, ZZZ)
+ZZZ = (ZZZ, ZZZ)".Replace("\r", "").Split("\n")*)
+
+let instructions = lines[0]
+
+let mapLines =
+    lines
+    |> Seq.skip 2
+
+let map =
+    mapLines
+    |> Seq.map Location
+    |> Seq.map (fun location -> location.Name, location)
+    |> dict
+
+printfn "%A" map
+
+let mutable steps = 0
+let mutable nextInstruction = 0
+let mutable currentLocation = "AAA"
+
+while currentLocation <> "ZZZ" do
+    let instruction = instructions[nextInstruction]
+    nextInstruction <- (nextInstruction + 1) % instructions.Length
+    currentLocation <- if instruction = 'L' then map[currentLocation].Left else map[currentLocation].Right
+    steps <- steps + 1
+
+printfn "[Part 1] Number of steps to reach ZZZ = %d" steps
