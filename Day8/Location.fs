ed53d1a0617c0bef94aaec77f0952fb0cef8407f
codereader <greebo@angua.at>
2023-12-09 18:15:53 +0100

    Failing Part 2

--- Day8/Location.fs
+++ Day8/Location.fs
@@ -1,11 +1,14 @@
 ﻿namespace Day8
 
-type Location(input:string) =
-    let Pieces = input.Split(" = ")
-    let LR_Pieces = Pieces[1].Split(", ")
-    member this.Name = Pieces[0]
-    member this.Left = LR_Pieces[0].Replace("(", "").Replace(")", "")
-    member this.Right = LR_Pieces[1].Replace("(", "").Replace(")", "")
+type Location(name, left, right) =
+    member this.Name = name
+    member this.Left = left
+    member this.Right = right
+
+    new (input:string) =
+        let Pieces = input.Split(" = ")
+        let LR_Pieces = Pieces[1].Split(", ")
+        Location(Pieces[0], LR_Pieces[0].Replace("(", "").Replace(")", ""), LR_Pieces[1].Replace("(", "").Replace(")", ""))
 
     override this.ToString() =
         sprintf "%s (L: %s | R: %s)" this.Name this.Left this.Right
--- Day8/Program.fs
+++ Day8/Program.fs
@@ -2,13 +2,18 @@
 open System
 
 // Real puzzle input
-let lines = IO.File.ReadAllLines @"..\..\..\input.txt"
+//let lines = IO.File.ReadAllLines @"..\..\..\input.txt"
 // test input
-(*let lines = "LLR
+let lines = "LR
 
-AAA = (BBB, BBB)
-BBB = (AAA, ZZZ)
-ZZZ = (ZZZ, ZZZ)".Replace("\r", "").Split("\n")*)
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)".Replace("\r", "").Split("\n")
 
 let instructions = lines[0]
 
@@ -24,14 +29,36 @@ let map =
 
 printfn "%A" map
 
-let mutable steps = 0
-let mutable nextInstruction = 0
-let mutable currentLocation = "AAA"
+let RunPart1 = 
+    let mutable steps = 0
+    let mutable nextInstruction = 0
+    let mutable currentLocation = "AAA"
 
-while currentLocation <> "ZZZ" do
+    while currentLocation <> "ZZZ" do
+        let instruction = instructions[nextInstruction]
+        nextInstruction <- (nextInstruction + 1) % instructions.Length
+        currentLocation <- if instruction = 'L' then map[currentLocation].Left else map[currentLocation].Right
+        steps <- steps + 1
+
+    steps
+
+//printfn "[Part 1] Number of steps to reach ZZZ = %d" RunPart1
+
+let mutable steps = 0L
+let mutable nextInstruction = 0
+let mutable currentLocations = map.Keys |> Seq.filter (fun name -> name[name.Length - 1] = 'A') |> Seq.toArray
+
+let GoalReached =
+    currentLocations |> Seq.exists (fun name -> name[name.Length - 1] <> 'Z') = false
+
+while (not GoalReached) do
     let instruction = instructions[nextInstruction]
     nextInstruction <- (nextInstruction + 1) % instructions.Length
-    currentLocation <- if instruction = 'L' then map[currentLocation].Left else map[currentLocation].Right
-    steps <- steps + 1
+    for i in { 0..currentLocations.Length - 1 } do
+        currentLocations[i] <- if instruction = 'L' then map[currentLocations[i]].Left else map[currentLocations[i]].Right
+    steps <- steps + 1L
+    if steps % 10000000L = 0L then
+        printfn "%d steps reached" steps
 
-printfn "[Part 1] Number of steps to reach ZZZ = %d" steps
+printfn "[Part 2] Number of steps to reach ZZZ = %d" steps
+
