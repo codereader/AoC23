85b8c872609e96631293ed9655cd44af4ad8c381
codereader <greebo@angua.at>
2023-12-09 14:35:49 +0100

    Refactoring

--- Day7/Program.fs
+++ Day7/Program.fs
@@ -1,11 +1,12 @@
 ﻿open System
 
+// Real puzzle input
 let lines = IO.File.ReadAllLines @"..\..\..\input.txt"
 
 // Test input
 //let lines = [| "32T3K 765"; "T55J5 684"; "KK677 28"; "KTJJT 220"; "QQQJA 483" |]
 
-type HandClass =
+type HandType =
     | FiveOfAKind = 7
     | FourOfAKind = 6
     | FullHouse = 5
@@ -17,19 +18,19 @@ type HandClass =
 type Hand(hand:string, bid: int) =
     member val Hand = hand with get
     member val Bid = bid with get
-    member this.Class = this.DetermineClass(this.Hand)
+    member this.Type = this.DetermineType(this.Hand)
 
-    // The numeric value representing the hand's strength
-    member this.HexValue = this.CalculateHexValue(this.Hand)
+    // The numeric value representing the hand's strength, used for breaking ties
+    member this.Value = this.CalculateHexValue(this.Hand)
 
     // Calculated rank after sorting, gets assigned from outside
     member val Rank = 0 with get, set
 
     override this.ToString() =
-        sprintf "%s %d [%A] (Value %s, Rank %d)" this.Hand this.Bid this.Class (this.GetHexStringForHand(this.Hand)) this.Rank
+        sprintf "%s %d [%A] (Value %s, Rank %d)" this.Hand this.Bid this.Type (this.GetHexStringForHand(this.Hand)) this.Rank
 
-    // Determine the hand's class based on the given string
-    member this.DetermineClass(hand: seq<char>) =
+    // Determine the hand's type based on the given string
+    member this.DetermineType(hand: seq<char>) =
         let counts =
             hand
             |> Seq.groupBy (fun ch -> ch)
@@ -37,17 +38,17 @@ type Hand(hand:string, bid: int) =
             |> Seq.toList
 
         if counts |> Seq.exists (fun i -> i = 5) then
-            HandClass.FiveOfAKind
+            HandType.FiveOfAKind
         else if counts |> Seq.exists (fun i -> i = 4) then
-            HandClass.FourOfAKind
+            HandType.FourOfAKind
         else if counts |> Seq.exists (fun i -> i = 3)  then
-            if counts |> Seq.exists (fun i -> i = 2) then HandClass.FullHouse else HandClass.ThreeOAKind
+            if counts |> Seq.exists (fun i -> i = 2) then HandType.FullHouse else HandType.ThreeOAKind
         else if counts |> Seq.filter (fun i -> i = 2) |> Seq.length = 2 then
-            HandClass.TwoPair
+            HandType.TwoPair
         else if counts |> Seq.exists (fun i -> i = 2) then
-            HandClass.OnePair
+            HandType.OnePair
         else
-            HandClass.HighCard
+            HandType.HighCard
 
     member this.CalculateHexValue(hand: seq<char>) =
         Convert.ToInt32(this.GetHexStringForHand(hand: seq<char>), 16)
@@ -85,36 +86,32 @@ type Part2Hand (hand:string, bid: int) =
     inherit Hand(hand, bid)
 
     member val HandWithJokersFilledIn = "" with get, set
-    member val ClassWithJokersFilledIn = HandClass.HighCard with get, set
-    member val ValueWithJokersFilledIn = 0 with get, set
-    member val RankWithJokersFilledIn = 0 with get, set
+    member val TypeWithJokersFilledIn = HandType.HighCard with get, set
 
     member this.DetermineBestValue() =
         Console.Write("Checking {0}...", this.Hand)
 
+        // Get the string positions the jokers are sitting at
         let jokerSlots =
             seq { 0..this.Hand.Length-1 }
             |> Seq.filter (fun i -> this.Hand[i] = 'J')
             |> Seq.toArray
 
         if jokerSlots.Length > 0 then
-            Console.Write("...Jokers at {0}...", String.Join(',', jokerSlots))
+            Console.Write("...Jokers at ({0})...", String.Join(',', jokerSlots))
 
+        // Use a brute-force algorithm to cover all possible combinations of joker replacements
         let bestCombo =
             GetCombinations [] jokerSlots.Length AllCardsWithoutJoker
-            // Distribute the combinations to the available slots and check that hand's strength
+            // Distribute the combinations to the available slots and check that hand's type
             |> Seq.map (fun combo -> this.FillJokers(jokerSlots, combo))
-            |> Seq.map (fun combo -> (combo, (this.DetermineClass combo), (this.CalculateHexValue combo)))
+            |> Seq.map (fun combo -> (combo, (this.DetermineType combo), (this.CalculateHexValue combo)))
             |> Seq.sortByDescending (fun (_,c,value) -> c, value)
 
         let (combo, cls, value) = bestCombo |> Seq.head
 
         this.HandWithJokersFilledIn <- new String(combo)
-        this.ValueWithJokersFilledIn <- value
-        this.ClassWithJokersFilledIn <- cls
-
-        //combos
-        //    |> Seq.iter (fun combo -> printfn "%A" combo)
+        this.TypeWithJokersFilledIn <- cls
 
         Console.WriteLine("found {0}", this.HandWithJokersFilledIn)
 
@@ -135,7 +132,7 @@ type Part2Hand (hand:string, bid: int) =
         | _ -> ch
 
     override this.ToString() =
-        sprintf "%s [Optimised: %s, Rank: %d, Class: %A]" (base.ToString()) this.HandWithJokersFilledIn this.RankWithJokersFilledIn this.ClassWithJokersFilledIn
+        sprintf "%s [Optimised: %s, Class: %A]" (base.ToString()) this.HandWithJokersFilledIn this.TypeWithJokersFilledIn
 
 
 let hands =
@@ -145,7 +142,7 @@ let hands =
 
 let sortedHands =
     hands
-    |> Seq.sortBy (fun hand -> hand.Class, hand.HexValue)
+    |> Seq.sortBy (fun hand -> hand.Type, hand.Value)
     |> Seq.toArray
 
 seq { 0..sortedHands.Length - 1 }
@@ -160,28 +157,33 @@ let totalWinnings =
 
 printfn "[Part 1]: Total Winnings: %d" totalWinnings
 
+// Part 2: Re-parse the input, this time we're using a new Hand class type (to get the hang of inheritance)
 let part2Hands =
     lines
     |> Seq.map (fun line -> line.Split(' ', StringSplitOptions.RemoveEmptyEntries))
     |> Seq.map (fun arr -> Part2Hand(arr[0], int arr[1]))
     |> Seq.toArray
 
+// Determine the card's best possible type by filling in the jokers
 part2Hands
     |> Seq.iter (fun hand -> hand.DetermineBestValue())
 
+// Careful when sorting the new hands, only the card type is using jokers, the value is the same
+// "However, for the purpose of breaking ties between two hands of the same type, J is always treated as J, not the card it's pretending to be"
 let part2SortedHands =
     part2Hands
-    |> Seq.sortBy (fun hand -> hand.ClassWithJokersFilledIn, hand.HexValue) // take the original value without jokers filled in
+    |> Seq.sortBy (fun hand -> hand.TypeWithJokersFilledIn, hand.Value) // use the original value to break ties
     |> Seq.toArray
 
+// Assign rank
 seq { 0..part2SortedHands.Length - 1 }
-    |> Seq.iter (fun i -> part2SortedHands[i].RankWithJokersFilledIn <- i+1)
+    |> Seq.iter (fun i -> part2SortedHands[i].Rank <- i+1)
 
 part2SortedHands
     |> Seq.iter (fun hand -> printfn "%A" hand)
 
 let part2TotalWinnings =
     part2SortedHands
-    |> Seq.sumBy (fun hand -> (int64 hand.RankWithJokersFilledIn) * (int64 hand.Bid))
+    |> Seq.sumBy (fun hand -> hand.Rank * hand.Bid)
 
 printfn "[Part 2]: Total Winnings: %d" part2TotalWinnings
