2bed9cc801851bd05f3180c71e4213d02e081f96
codereader <greebo@angua.at>
2023-12-10 06:58:46 +0100

    Day 9 Part 2

--- Day9/Program.fs
+++ Day9/Program.fs
@@ -10,7 +10,7 @@ let sequences =
     |> Seq.toArray
 
 // The next interpolated value for the given sequence
-let rec GetExtrapolatedValue(sequence: int array) =
+let rec GetFutureExtrapolatedValue(sequence: int array) =
     let mutable differenceSum = 0
     let differences = 
         { 1..sequence.Length - 1 }
@@ -21,12 +21,31 @@ let rec GetExtrapolatedValue(sequence: int array) =
         |> Seq.toArray
 
     let childInterpolation =
-        if differenceSum = 0 then 0 else GetExtrapolatedValue differences
+        if differenceSum = 0 then 0 else GetFutureExtrapolatedValue differences
 
     sequence[sequence.Length - 1] + childInterpolation
 
-let extrapolations = sequences |> Seq.map GetExtrapolatedValue
-
+let extrapolations = sequences |> Seq.map GetFutureExtrapolatedValue
 let extrapolationSum = extrapolations |> Seq.sum
 
-printfn "[Part 1]: Sum of all extrapolated values: %d" extrapolationSum
+printfn "[Part 1]: Sum of all future extrapolated values: %d" extrapolationSum
+
+let rec GetPastExtrapolatedValue(sequence: int array) =
+    let mutable differenceSum = 0
+    let differences = 
+        { 1..sequence.Length - 1 }
+        |> Seq.map (fun i -> sequence[i] - sequence[i-1])
+        |> Seq.map (fun i ->
+            differenceSum <- differenceSum + i
+            i)
+        |> Seq.toArray
+
+    let childInterpolation =
+        if differenceSum = 0 then 0 else GetPastExtrapolatedValue differences
+
+    sequence[0] - childInterpolation
+
+let pastExtrapolations = sequences |> Seq.map GetPastExtrapolatedValue
+let pastExtrapolationSum = pastExtrapolations |> Seq.sum
+
+printfn "[Part 2]: Sum of all past extrapolated values: %d" pastExtrapolationSum
