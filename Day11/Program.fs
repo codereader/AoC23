d60556076def8e64d125993928610f8c5e4e779c
codereader <greebo@angua.at>
2023-12-11 08:48:48 +0100

    Day 11 Part 2

--- Day11/Program.fs
+++ Day11/Program.fs
@@ -17,6 +17,7 @@ lines <- "...#......
 #...#.....".Replace("\r\n", "\n").Split('\n')
 *)
 let originalWidth = lines[0].Length
+let originalHeight = lines.Length
 
 // Find columns without any galaxies
 let columnsWithoutGalaxies = 
@@ -71,7 +72,7 @@ let galaxies =
     |> Seq.map Vector2
     |> Seq.toList
 
-galaxies |> Seq.iter (fun pos -> printfn "%A" pos)
+//galaxies |> Seq.iter (fun pos -> printfn "%A" pos)
 
 let galaxyPairs =
     seq {
@@ -91,3 +92,50 @@ let totalDistanceSum =
     |> Seq.sumBy CalculateDistance
 
 printfn "[Part 1]: Total Distance = %d" totalDistanceSum
+
+let orignalGalaxies =
+    seq {
+        for row in { 0.. originalHeight-1 } do
+            for col in { 0.. originalWidth-1 } do
+                if lines[row][col] = '#' then
+                    yield (col,row) }
+    |> Seq.map Vector2
+    |> Seq.toList
+
+let originalGalaxyPairs =
+    seq {
+    for i in { 0..orignalGalaxies.Length-1 } do
+        for j in { i+1..orignalGalaxies.Length-1 } do
+            yield (orignalGalaxies[i], orignalGalaxies[j]) }
+    |> Seq.toArray
+
+printfn "%d pairs" originalGalaxyPairs.Length
+
+type Range(a:int, b:int) =
+    member this.Min = System.Math.Min(a, b)
+    member this.Max = System.Math.Max(a, b)
+    member this.Distance = System.Math.Abs(a - b)
+    member this.Contains(value) = this.Min <= value && this.Max >= value
+
+let CalculateDistancePart2((galaxy1: Vector2, galaxy2: Vector2)) =
+    let rangeX = Range(galaxy1.X, galaxy2.X)
+    let rangeY = Range(galaxy1.Y, galaxy2.Y)
+
+    let xCrossings = 
+        columnsWithoutGalaxies
+        |> Seq.filter (fun col -> rangeX.Contains col)
+        |> Seq.length
+
+    let yCrossings = 
+        rowsWithoutGalaxies
+        |> Seq.filter (fun row -> rangeY.Contains row)
+        |> Seq.length
+
+    rangeX.Distance + rangeY.Distance + xCrossings * 999999 + yCrossings * 999999
+
+let totalDistancePart2 = 
+    originalGalaxyPairs
+    |> Seq.map CalculateDistancePart2
+    |> Seq.sumBy (fun dist -> uint64 dist)
+
+printfn "[Part 2]: Total Distance = %d" totalDistancePart2
