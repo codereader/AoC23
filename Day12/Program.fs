00eaa3c74b3a932da5e3c3b7c13c0584f445b649
codereader <greebo@angua.at>
2023-12-12 06:55:53 +0100

    Day 12 Part 1

---
+++ Day12/Program.fs
@@ -0,0 +1,60 @@
+﻿open System
+open Utils
+open System.Collections.Generic
+
+// Real puzzle input
+let mutable lines = IO.File.ReadAllLines @"..\..\..\input.txt"
+
+(*
+lines <- "???.### 1,1,3
+.??..??...?##. 1,1,3
+?#?#?#?#?#?#?#? 1,3,1,6
+????.#...#... 4,1,1
+????.######..#####. 1,6,5
+?###???????? 3,2,1".Replace("\r\n", "\n").Split('\n')
+*)
+
+let LineIsValid(line:string) =
+    let pieces = line.Split(' ')
+    let value = pieces[0]
+    let requiredLengths = pieces[1].Split(',') |> Seq.map int |> Seq.toArray
+    let faultyLengths = value.Split('.', StringSplitOptions.RemoveEmptyEntries) |> Seq.map (fun v -> v.Length )
+
+    (Seq.compareWith Operators.compare faultyLengths requiredLengths) = 0
+
+let FillComboIntoPlaceholders(line:char array, indices: int array, combo: char array) =
+    let copy = line.Clone() :?> char array
+    if combo.Length <> indices.Length then failwith "Invalid sizes, combo must be the same length as indices"
+    seq { 0..indices.Length - 1}
+        |> Seq.iter (fun n -> copy[indices[n]] <- combo[n])
+    new String(copy)
+
+let CalculatePossibilities(line) =
+    printfn "%s" line
+
+    let indices = seq { 0..line.Length - 1 } |> Seq.filter (fun i -> line[i] = '?') |> Seq.toArray
+
+    let combos = Algorithm.GetPermutations [] indices.Length ['#';'.']
+    
+    //combos |> Seq.iter (fun combo -> printfn "  Combo: %A" combo)
+
+    // Fill in the combos
+    let validCombos = 
+        combos
+        |> Seq.map (fun combo -> 
+            let filled = FillComboIntoPlaceholders(line.ToCharArray(), indices, combo)
+            //printfn "%s = %A" filled (LineIsValid(filled))
+            filled
+        )
+        |> Seq.filter LineIsValid
+        |> Seq.length
+
+    printfn "  Valid Combo Count: %A" validCombos
+
+    validCombos
+
+let part1Sum =
+    lines 
+    |> Seq.sumBy CalculatePossibilities
+
+printfn "[Part 1]: Sum of all valid possibilities = %d" part1Sum
---
+++ Utils/Algorithm.fs
@@ -0,0 +1,25 @@
+﻿module Algorithm
+
+// Yield all combinations of the given set with N free slots
+// Usage: e.g. GetCombinations [] 3 ['4';'3';'2']
+// will create all 3-digit combinations, picking from the set [4,3,2]
+// Only creates one combination for each set of picks, no permutations
+let rec GetCombinations acc size set = seq {
+  match size, set with
+  | n, x::xs ->
+      if n > 0 then yield! GetCombinations (x::acc) (n - 1) set
+      if n >= 0 then yield! GetCombinations acc n xs
+  | 0, [] -> yield acc |> Seq.toArray
+  | _, [] -> () }
+
+// Yield all combinations including permutations
+let rec GetPermutations acc size set = seq {
+  match size, set with
+  | n, head::tail ->
+      if n > 0 then 
+        for item in set do
+            yield! GetPermutations (item::acc) (n - 1) set
+      if n = 0 then
+        yield! GetPermutations acc n tail
+  | 0, [] -> yield acc |> Seq.toArray
+  | _, [] -> () }
