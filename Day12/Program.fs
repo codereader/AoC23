96e78c6fc18c8af85887bab9b119559bb33ada7e
codereader <greebo@angua.at>
2023-12-13 09:25:31 +0100

    Day 12 Part 2 ongoing

--- Day12/Program.fs
+++ Day12/Program.fs
@@ -5,14 +5,13 @@ open System.Collections.Generic
 // Real puzzle input
 let mutable lines = IO.File.ReadAllLines @"..\..\..\input.txt"
 
-(*
 lines <- "???.### 1,1,3
 .??..??...?##. 1,1,3
 ?#?#?#?#?#?#?#? 1,3,1,6
 ????.#...#... 4,1,1
 ????.######..#####. 1,6,5
 ?###???????? 3,2,1".Replace("\r\n", "\n").Split('\n')
-*)
+
 
 let LineIsValid(line:string) =
     let pieces = line.Split(' ')
@@ -58,3 +57,57 @@ let part1Sum =
     |> Seq.sumBy CalculatePossibilities
 
 printfn "[Part 1]: Sum of all valid possibilities = %d" part1Sum
+
+let Times5(list) = list @ list @ list @ list @ list
+
+let CalculatePossibilitiesPart2(line:string) =
+
+    let pieces = line.Split(' ')
+
+    let values = String.Join('?', seq { pieces[0]; pieces[0]; pieces[0]; pieces[0]; pieces[0] } |> Seq.toList)
+    let groupLengths = pieces[1].Split(',') |> Seq.map int |> Seq.toList |> Times5
+
+    printfn "%s %s" values (String.Join(',', groupLengths))
+
+    let rec GenerateSubCombinations(fix:string, rest:string) = seq {
+        
+        // Reached the bottom of it
+        if rest.Length = 0 then
+            yield fix
+
+        //printfn "%s | %s" fix rest
+
+        let nextWildCard = rest.IndexOf('?')
+
+        if nextWildCard = -1 then
+            yield fix + rest
+        else
+            let nonWildCards = if nextWildCard = 0 then "" else rest.Substring(0, nextWildCard)
+            let left = fix + nonWildCards + "#"
+            let right = fix + nonWildCards + "."
+
+            if rest.Length = 1 then
+                yield left
+                yield right
+            else
+                for subresult in GenerateSubCombinations(left, rest.Substring(nextWildCard + 1)) do
+                    yield subresult
+                for subresult in GenerateSubCombinations(right, rest.Substring(nextWildCard + 1)) do
+                    yield subresult
+    }
+
+    let combos = GenerateSubCombinations("", values)
+
+    //combos |> Seq.iter (fun combo -> printfn "%s" combo)
+
+    let validCombos = combos |> Seq.length
+    
+    printfn " Part 2 Valid Combo Count: %A" validCombos
+
+    validCombos
+
+let part2sum =
+    lines
+    |> Seq.sumBy CalculatePossibilitiesPart2
+
+printfn "[Part 2]: Sum of all valid possibilities = %d" part2sum
