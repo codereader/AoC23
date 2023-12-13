c3e504b1132499cbef1eab2530a1590bb478f899
codereader <greebo@angua.at>
2023-12-13 15:21:19 +0100

    Day 12 Part 2 ongoing

--- Day12/Program.fs
+++ Day12/Program.fs
@@ -1,6 +1,7 @@
 ﻿open System
 open Utils
 open System.Collections.Generic
+open System.Diagnostics
 
 // Real puzzle input
 let mutable lines = IO.File.ReadAllLines @"..\..\..\input.txt"
@@ -69,40 +70,67 @@ let CalculatePossibilitiesPart2(line:string) =
 
     printfn "%s %s" values (String.Join(',', groupLengths))
 
+    // Yields only completed groups
+    let CompletedGroupsInLine(line:string) = seq {
+        let mutable hashStartIndex = line.IndexOf('#')
+
+        while hashStartIndex <> -1 do
+            let hashEndIndex = line.IndexOf('.', hashStartIndex)
+
+            (*if hashEndIndex = -1 && line[line.Length - 1] = '#' then
+                yield line.Length - hashStartIndex // final group
+                hashStartIndex <- -1
+            else *)
+            if hashEndIndex <> -1 then
+                yield hashEndIndex - hashStartIndex
+                hashStartIndex <- line.IndexOf('#', hashEndIndex)
+            else
+                hashStartIndex <- -1
+        }
+
+    let LineIsFullyValid(line:string) =
+        let pieces = line.Split(' ')
+        let value = pieces[0]
+        let faultyLengths = value.Split('.', StringSplitOptions.RemoveEmptyEntries) |> Seq.map (fun v -> v.Length )
+
+        (Seq.compareWith Operators.compare faultyLengths groupLengths) = 0
+
+    let LineMightBeValid(line:string) =
+        let groupsInLine = CompletedGroupsInLine line
+        Seq.forall2 (fun group requirement ->
+            //printfn "Req: %d Group %d" requirement group
+            requirement >= group) groupsInLine groupLengths
+
     let rec GenerateSubCombinations(fix:string, rest:string) = seq {
         
-        // Reached the bottom of it
-        if rest.Length = 0 then
-            yield fix
-
-        //printfn "%s | %s" fix rest
-
         let nextWildCard = rest.IndexOf('?')
 
         if nextWildCard = -1 then
-            yield fix + rest
+            if LineIsFullyValid(fix + rest) then 1 else 0
         else
             let nonWildCards = if nextWildCard = 0 then "" else rest.Substring(0, nextWildCard)
             let left = fix + nonWildCards + "#"
             let right = fix + nonWildCards + "."
 
-            if rest.Length = 1 then
-                yield left
-                yield right
-            else
-                for subresult in GenerateSubCombinations(left, rest.Substring(nextWildCard + 1)) do
-                    yield subresult
-                for subresult in GenerateSubCombinations(right, rest.Substring(nextWildCard + 1)) do
-                    yield subresult
+            if LineMightBeValid(left) then
+                GenerateSubCombinations(left, rest.Substring(nextWildCard + 1)) |> Seq.sum
+
+            if LineMightBeValid(right) then
+                GenerateSubCombinations(right, rest.Substring(nextWildCard + 1)) |> Seq.sum
     }
 
-    let combos = GenerateSubCombinations("", values)
+    let stopWatch = new Stopwatch()
+    stopWatch.Start()
+
+    let combos = GenerateSubCombinations("", values) |> Seq.sum
 
     //combos |> Seq.iter (fun combo -> printfn "%s" combo)
 
-    let validCombos = combos |> Seq.length
+    let validCombos = combos
+
+    stopWatch.Stop()
     
-    printfn " Part 2 Valid Combo Count: %A" validCombos
+    printfn " => Combo Count: %A in %f seconds" validCombos (stopWatch.Elapsed.TotalSeconds)
 
     validCombos
 
