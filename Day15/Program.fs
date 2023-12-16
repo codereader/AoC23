2f37e984caef7553c17d3b7d4d3f8a3ca7ac6650
codereader <greebo@angua.at>
2023-12-16 10:05:42 +0100

    Day 15 Part 1

---
+++ Day15/Program.fs
@@ -0,0 +1,25 @@
+﻿open System
+
+// Real puzzle input
+let mutable lines = IO.File.ReadAllLines @"..\..\..\input.txt"
+
+// Test input
+//lines <- "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7".Replace("\r\n", "\n").Split('\n')
+
+let sequence = (lines |> Seq.head).Split(',', StringSplitOptions.RemoveEmptyEntries)
+
+let CalculateHash(input:string) =
+    let mutable hash = 0
+    input |> Seq.iter (fun ch -> hash <- ((hash + int ch) * 17) % 256)
+    hash
+
+printfn "Hash of 'HASH' = %d" (CalculateHash "HASH")
+
+let sequenceHash = 
+    sequence
+    |> Seq.map CalculateHash
+    |> Seq.sum
+
+// Correct answer is 512950
+printfn "[Part 1] Intialization Sequence Hash = %d"  sequenceHash
+
