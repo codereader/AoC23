430a62098c3d1c6db1920cf58fda5a98d22d5f05
codereader <greebo@angua.at>
2023-12-28 12:49:17 +0100

    Day 19 Part 2

--- Day19/Program.fs
+++ Day19/Program.fs
@@ -47,7 +47,7 @@ let parts =
 
 type WorkflowStep = { Condition: Part -> bool; Action: Part -> unit }
 
-type Workflow = { Name: string; Steps: WorkflowStep list }
+type WorkflowPart1 = { Name: string; Steps: WorkflowStep list }
 
 let ParseSelector(input: string) =
     match input[0] with
@@ -91,7 +91,7 @@ let ParseAction(input: string) =
     else
         ParseTarget(input)
 
-let workflows = 
+let workflowsPart1 = 
     workflowLines
     |> Seq.map (fun line -> 
             let nameAndRest = line.Split('{')
@@ -116,7 +116,7 @@ let workflows =
 let EvaluatePart(part:Part) =
     printf "Part: %s" part.CurrentWorkflow
     while part.Accepted.IsNone do
-        let workflow = workflows[part.CurrentWorkflow]
+        let workflow = workflowsPart1[part.CurrentWorkflow]
         
         let applicableStep = workflow.Steps |> Seq.find (fun step -> step.Condition(part))
         applicableStep.Action(part)
@@ -129,3 +129,145 @@ let partSum =
     |> Seq.sumBy (fun part -> part.X + part.M + part.A + part.S)
 
 printfn "[Part 1]: Sum of all classifications of accepted parts = %d" partSum
+
+(* ============================================================================== *)
+
+type ValidRange(min, max) =
+    member this.Min = min
+    member this.Max = max
+    member this.IsValid =
+        this.Max >= this.Min
+    member this.Clone() =
+        ValidRange(min, max)
+
+type Part2(x, m, a, s, workflow, stepIndex) =
+    member this.X = x
+    member this.M = m
+    member this.A = a
+    member this.S = s
+    member val CurrentWorkflow = workflow with get,set
+    member val StepIndex = stepIndex with get, set
+
+    new() =
+        Part2(ValidRange(1, 4000), ValidRange(1, 4000), ValidRange(1, 4000), ValidRange(1, 4000), "in", 0)
+
+    member this.ReplaceRange(cls: char, range) =
+        match cls with
+        | 'x' -> Part2(range, this.M.Clone(), this.A.Clone(), this.S.Clone(), this.CurrentWorkflow, this.StepIndex)
+        | 'm' -> Part2(this.X.Clone(), range, this.A.Clone(), this.S.Clone(), this.CurrentWorkflow, this.StepIndex)
+        | 'a' -> Part2(this.X.Clone(), this.M.Clone(), range, this.S.Clone(), this.CurrentWorkflow, this.StepIndex)
+        | 's' -> Part2(this.X.Clone(), this.M.Clone(), this.A.Clone(), range, this.CurrentWorkflow, this.StepIndex)
+        | _ -> failwith "???"
+
+type WorkflowPart2 = { Name: string; Steps: string list }
+
+let workflowsPart2 = 
+    workflowLines
+    |> Seq.map (fun line -> 
+            let nameAndRest = line.Split('{')
+            let name = nameAndRest[0]
+
+            // s<1351:px,qqz
+            // s<537:gd,x>2440:R,A
+            let steps =
+                nameAndRest[1].Trim('}').Split(',')
+                |> Seq.toList
+
+            (name, { Name = name; Steps = steps })
+        )
+    |> Map.ofSeq
+
+let acceptedParts = new List<Part2>()
+
+let IntersectRange(range: ValidRange, target: ValidRange) =
+    let candidate = ValidRange(Math.Max(range.Min, target.Min), Math.Min(range.Max, target.Max))
+    if candidate.IsValid then Some(candidate) else None
+
+let SplitRange(range: ValidRange, operator: char, operand: int) =
+    
+    if operator = '>' then
+        let matchingRange = IntersectRange(range, ValidRange(operand + 1, 4000))
+        let mismatchingRange = IntersectRange(range, ValidRange(1, operand))
+        (matchingRange, mismatchingRange)
+    else
+        let matchingRange = IntersectRange(range, ValidRange(1, operand - 1))
+        let mismatchingRange = IntersectRange(range, ValidRange(operand, 4000))
+        (matchingRange, mismatchingRange)
+
+let SplitPart(part: Part2, selector: char, operator: char, operand: int) =
+
+    let range =
+        match selector with
+        | 'x' -> part.X
+        | 'm' -> part.M
+        | 'a' -> part.A
+        | 's' -> part.S
+        | _ -> failwith "???"
+
+    let (matchingRange, mismatchingRange) = SplitRange(range, operator, operand)
+
+    (
+        (if matchingRange.IsSome then Some(part.ReplaceRange(selector, matchingRange.Value)) else None),
+        (if mismatchingRange.IsSome then Some(part.ReplaceRange(selector, mismatchingRange.Value)) else None)
+    )
+
+
+let EvaluateStep(part: Part2) =
+    let workflow = workflowsPart2[part.CurrentWorkflow]
+    let step = workflow.Steps[part.StepIndex]
+
+    if step = "A" || step = "R" then
+        seq { Part2(part.X, part.M, part.A, part.S, step, 0) }
+    else if step.Contains(':') then
+        // Evaluate the condition, this yields two branches
+        let parts = step.Split(':')
+        let target = parts[1]
+        let conditionStr = parts[0]
+
+        let operand = int (conditionStr.Split([| '<'; '>' |])[1])
+        let operator = if step.Contains('>') then '>' else '<'
+
+        let (matchingPart, mismatchingPart) = SplitPart(part, conditionStr[0], operator, operand)
+
+        seq { 
+            if matchingPart.IsSome then
+                matchingPart.Value.CurrentWorkflow <- target
+                matchingPart.Value.StepIndex <- 0
+                yield matchingPart.Value
+
+            if mismatchingPart.IsSome then
+                mismatchingPart.Value.StepIndex <- mismatchingPart.Value.StepIndex + 1
+                yield mismatchingPart.Value
+        }
+    else
+        // forward to the first step of another workflow
+        seq { Part2(part.X, part.M, part.A, part.S, step, 0) }
+
+let rootPart = new Part2()
+
+let mutable partsToInvestigate = new Stack<Part2>()
+
+partsToInvestigate.Push(rootPart)
+
+while partsToInvestigate.Count > 0 do
+    let part = partsToInvestigate.Pop()
+
+    for newPart in EvaluateStep(part) do
+
+        if newPart.CurrentWorkflow = "A" then
+            acceptedParts.Add(newPart)
+        else if newPart.CurrentWorkflow = "R" then
+            () // Reject
+        else
+            partsToInvestigate.Push(newPart)
+
+printfn "[Part 2] Accepted variants: %d" acceptedParts.Count
+
+let combinationCount =
+    acceptedParts
+    |> Seq.sumBy (fun part -> int64 (part.X.Max - part.X.Min + 1) *
+                              int64 (part.M.Max - part.M.Min + 1) *
+                              int64 (part.A.Max - part.A.Min + 1) *
+                              int64 (part.S.Max - part.S.Min + 1))
+
+printfn "[Part 2] Acceptable combinations: %d" combinationCount
