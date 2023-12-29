a5a29d177ad8284cf7e20d24f138d85d92d0b78e
codereader <greebo@angua.at>
2023-12-29 05:08:19 +0100

    Day 21 Part 2 WIP

--- Day21/Program.fs
+++ Day21/Program.fs
@@ -6,7 +6,6 @@ open System.Diagnostics
 // Real puzzle input
 let mutable lines = IO.File.ReadAllLines @"..\..\..\input.txt"
 
-(*
 // Test input
 lines <- @"...........
 .....###.#.
@@ -19,7 +18,7 @@ lines <- @"...........
 .##.#.####.
 .##..##.##.
 ...........".Replace("\r\n", "\n").Split('\n')
-*)
+
 
 let grid =
     lines
@@ -37,34 +36,42 @@ let PrintGrid(grid: char array array, reachablePositions: HashSet<Vector2>) =
     let oldColour = Console.ForegroundColor
     let gridSize = Vector2(grid[0].Length, grid.Length)
 
-    for y in { 0..gridSize.Y-1 } do
-        for x in { 0..gridSize.X-1 } do
+    let maxX = reachablePositions |> Seq.map (fun p -> p.X) |> Seq.max
+    let maxY = reachablePositions |> Seq.map (fun p -> p.Y) |> Seq.max
+    let minX = reachablePositions |> Seq.map (fun p -> p.X) |> Seq.min
+    let minY = reachablePositions |> Seq.map (fun p -> p.Y) |> Seq.min
+
+    for y in { minY..maxY } do
+        for x in { minX..maxX } do
             if reachablePositions.Contains(Vector2(x,y)) then
                 Console.ForegroundColor <- ConsoleColor.White
                 printf "%c" 'O'
             else
                 Console.ForegroundColor <- ConsoleColor.DarkGray
-                printf "%c" (grid[y][x])
+                let my = y % gridSize.Y
+                let mx = x % gridSize.X
+                printf "%c" (grid[if my < 0 then my + gridSize.Y else my][if mx < 0 then mx + gridSize.X else mx])
         printfn ""
 
     Console.ForegroundColor <- oldColour
 
 let mutable reachablePositions = new HashSet<Vector2>()
-reachablePositions.Add(startPos)
-
-PrintGrid(grid, reachablePositions)
+ignore (reachablePositions.Add(startPos))
 
 let NESW = [| Vector2.Up; Vector2.Right; Vector2.Down; Vector2.Left |]
 let gridSize = Vector2(grid[0].Length, grid.Length)
 
-for round in { 1..64 } do
+for round in { 1..200 } do
     let newSet = new HashSet<Vector2>()
 
     for pos in  reachablePositions do
         NESW
             |> Seq.map (fun dir -> pos + dir)
-            |> Seq.filter (fun p -> p.X >= 0 && p.X < gridSize.X && p.Y >= 0 && p.Y < gridSize.Y)
-            |> Seq.filter (fun p -> grid[p.Y][p.X] <> '#')
+            //|> Seq.filter (fun p -> p.X >= 0 && p.X < gridSize.X && p.Y >= 0 && p.Y < gridSize.Y)
+            |> Seq.filter (fun p ->
+                let my = p.Y % gridSize.Y
+                let mx = p.X % gridSize.X
+                grid[if my < 0 then my + gridSize.Y else my][if mx < 0 then mx + gridSize.X else mx] <> '#')
             |> Seq.iter (fun p -> ignore (newSet.Add(p)))
         ()
 
@@ -72,3 +79,7 @@ for round in { 1..64 } do
 
 PrintGrid(grid, reachablePositions)
 printfn "[Part 1]: Reachable Positions = %d" reachablePositions.Count
+
+// Test Data:
+// N=200 => 26538
+
